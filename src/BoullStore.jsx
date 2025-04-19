7921889a13165dae102981a02b9f88cef37da2d7
boullstore <fajarazisyt@gmail.com>
2025-04-19 15:55:27 +0700

    Update BoullStore.jsx

--- src/BoullStore.jsx
+++ src/BoullStore.jsx
@@ -1,1 +1,11 @@
-(placeholder)
+import React from 'react';
+
+function BoullStore() {
+  return (
+    <div>
+      <h1>Selamat datang di Boull Store!</h1>
+    </div>
+  );
+}
+
+export default BoullStore;
